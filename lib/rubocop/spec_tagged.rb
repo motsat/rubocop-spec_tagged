e2f9cbb1289b0e774a586d239b90e20ffe8b2a89
motsat <konpeiex@gmail.com>
2018-06-08 01:18:22 +0900

    initial commit

---
+++ lib/rubocop/spec_tagged.rb
@@ -0,0 +1,7 @@
+require "rubocop/spec_tagged/version"
+
+module Rubocop
+  module SpecTagged
+    # Your code goes here...
+  end
+end
---
+++ lib/rubocop/spec_tagged/version.rb
@@ -0,0 +1,5 @@
+module Rubocop
+  module SpecTagged
+    VERSION = "0.1.0"
+  end
+end
